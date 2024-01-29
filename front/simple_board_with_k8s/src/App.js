72e12471c658e240a0af4f35ff075094015ddaef
akm4545 <akm4545@naver.com>
2024-01-29 15:17:06 +0900

    회원가입 및 로그인 페이지 레이아웃 작성

--- front/simple_board_with_k8s/src/App.js
+++ front/simple_board_with_k8s/src/App.js
@@ -3,6 +3,7 @@ import { Route, Navigate, Routes } from "../node_modules/react-router-dom/dist/i
 import MainPage from "./pages/MainPage";
 import BoardDetailPage from "./pages/BoardDetailPage";
 import BoardFormPage from "./pages/BoardFormPage";
+import MemberPage from "./pages/MemberPage";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -13,6 +14,8 @@ const App = () => {
         <Route path="/" element={<MainPage></MainPage>}></Route>
         <Route path="/boardDetail" element={<BoardDetailPage></BoardDetailPage>}></Route>
         <Route path="/boardForm" element={<BoardFormPage></BoardFormPage>}></Route>
+        <Route path="/memberJoin" element={<MemberPage></MemberPage>}></Route>
+        <Route path="/login" element={<MemberPage></MemberPage>}></Route>
       </Routes>
     </>
   )
---
+++ front/simple_board_with_k8s/src/pages/MemberPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/common/Header";
+import Body from "../components/common/Body";
+
+const MemberPage = () => {
+    return <>
+        <Header/>
+        <Body></Body>
+    </>
+};
+
+export default MemberPage;
