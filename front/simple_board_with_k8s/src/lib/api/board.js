08f0cdf5e1574276ebf5632a1b9efdb5349c95da
akm4545 <akm4545@naver.com>
2024-01-31 11:15:34 +0900

    api 주소 설정

--- front/simple_board_with_k8s/src/lib/api/board.js
+++ front/simple_board_with_k8s/src/lib/api/board.js
@@ -1,21 +1,21 @@
 import asyncRequestBase from "./asyncRequestBase";
 
 export const boardList = (page) => {
-    return asyncRequestBase("GET", ``);
+    return asyncRequestBase("GET", `/post?page=${page}`);
 };
 
 export const boardDetail = (seq) => {
-    return asyncRequestBase("GET", ``);
+    return asyncRequestBase("GET", `/post/${seq}`);
 };
 
 export const boardInsert = ({title, content}) => {
-    return asyncRequestBase("POST", ``);
+    return asyncRequestBase("POST", `/post`, {title, content});
 };
 
-export const boardUpdate = ({title, content}) => {
-    return asyncRequestBase("PUT", ``);
+export const boardUpdate = ({seq, title, content}) => {
+    return asyncRequestBase("PUT", `/post/${seq}`, {title, content});
 };
 
-export const boardDelete = (sqe) => {
-    return asyncRequestBase("DELETE", ``);
+export const boardDelete = (seq) => {
+    return asyncRequestBase("DELETE", `/post/${seq}`);
 };
