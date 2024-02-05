70bd0f0336e4dfdae4acb5ecf597c1aa98c0242b
akm4545 <akm4545@naver.com>
2024-02-05 12:38:20 +0900

    회원가입 컨테이너 작성 중

--- front/simple_board_with_k8s/src/components/member/MemberForm.js
+++ front/simple_board_with_k8s/src/components/member/MemberForm.js
@@ -2,28 +2,52 @@ import React from "react";
 import { Form } from "../../../node_modules/react-bootstrap/esm/index";
 import Button from 'react-bootstrap/Button';
 
-const MemberForm = ({joinFormSw}) => {
+const MemberForm = ({userId, userPw, userName, userPhone, joinFormSw, onChangeValue, writeBtnClick}) => {
     return <>
         <Form>
             <Form.Group className="mb-3" controlId="formGroupEmail">
                 <Form.Label>아이디</Form.Label>
-                <Form.Control name="userId" type="email" placeholder="아이디를 입력하세요" />
+                <Form.Control 
+                    name="userId" 
+                    value={userId} 
+                    type="email" 
+                    placeholder="아이디를 입력하세요"
+                    onChange={(e) => onChangeValue(e)}
+                />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formGroupPassword">
                 <Form.Label>비밀번호</Form.Label>
-                <Form.Control name="userPw" type="password" placeholder="비밀번호를 입력하세요." />
+                <Form.Control 
+                    name="userPw" 
+                    value={userPw} 
+                    type="password" 
+                    placeholder="비밀번호를 입력하세요." 
+                    onChange={(e) => onChangeValue(e)}
+                />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formGroupPassword">
                 <Form.Label>이름</Form.Label>
-                <Form.Control name="userName" type="text" placeholder="이름을 입력하세요." />
+                <Form.Control 
+                    name="userName" 
+                    value={userName}
+                    type="text" 
+                    placeholder="이름을 입력하세요." 
+                    onChange={(e) => onChangeValue(e)}
+                />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formGroupPassword">
                 <Form.Label>핸드폰번호</Form.Label>
-                <Form.Control name="userPhone" type="text" placeholder="핸드폰번호를 입력하세요." />
+                <Form.Control 
+                    name="userPhone" 
+                    value={userPhone}
+                    type="text" 
+                    placeholder="핸드폰번호를 입력하세요." 
+                    onChange={(e) => onChangeValue(e)}
+                />
             </Form.Group>
             <div style={{display: "flex", flexDirection: "row-reverse"}}>
                 {joinFormSw ? 
-                    <Button variant="outline-success">회원가입</Button> : 
+                    <Button variant="outline-success" onClick={(e) => writeBtnClick({userId, userPw, userName, userPhone, joinFormSw})}>회원가입</Button> : 
                     <Button variant="outline-success">로그인</Button>
                 }
             </div>
--- front/simple_board_with_k8s/src/containers/member/MemberContainer.js
+++ front/simple_board_with_k8s/src/containers/member/MemberContainer.js
@@ -1,15 +1,57 @@
 import React, {useEffect} from "react";
 import MemberForm from "../../components/member/MemberForm";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "../../../node_modules/react-router-dom/dist/index";
+import { changeValue } from "../../modules/member";
 
 const MemberContainer = () => {
     const location = useLocation().pathname;
     const joinFormSw = location === "/memberJoin";
 
+    const dispatch = useDispatch();
+
+    const {userId, userPw, userName, userPhone} = useSelector(({member}) => ({
+        userId: member.userId, 
+        userPw: member.userPw, 
+        userName: member.userName, 
+        userPhone: member.userPhone
+    }));
+
+    const writeBtnClick = ({userId, userPw, userName, userPhone, joinFormSw}) => {
+        if(!userId){
+            alert("아이디를 입력하세요.");
+            return;
+        }else if(!userPw){
+            alert("비밀번호를 입력하세요.");
+            return;
+        }
+
+        if(joinFormSw){
+            if(!userName){
+                alert("이름을 입력하세요.");
+                return;
+            }else if(!userPhone){
+                alert("핸드폰번호를 입력하세요.");
+                return;
+            }
+        }
+    };
+
+    const onChangeValue = (e) => {
+        const {name, value} = e.target;
+
+        dispatch(changeValue({key: name, value}));
+    };
+
     return <>
         <MemberForm
+            userId={userId}
+            userPw={userPw} 
+            userName={userName}
+            userPhone={userPhone}
             joinFormSw={joinFormSw}
+            onChangeValue={onChangeValue}
+            writeBtnClick={writeBtnClick}
         />
     </>;
 };
