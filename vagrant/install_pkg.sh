636aa5a2c72e07c609d6c2bc234739afe62c1a10
akm4545 <akm4545@naver.com>
2024-02-07 11:37:41 +0900

    베이크란트 파일 config.sh 호출 시 매개변수 삭제, install_pkg.sh 쿠버네티스 버전 명시

--- vagrant/install_pkg.sh
+++ vagrant/install_pkg.sh
@@ -11,12 +11,5 @@ yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-co
 systemctl enable --now docker
 
 # install kubernetes cluster 
-yum install kubectl-$1 kubelet-$1 kubeadm-$1 -y
+yum install kubectl-1.18.4 kubelet-1.18.4 kubeadm-1.18.4 -y
 systemctl enable --now kubelet
-
-# git clone _Book_k8sInfra.git 
-if [ $2 = 'Main' ]; then
-  git clone https://github.com/sysnet4admin/_Book_k8sInfra.git
-  mv /home/vagrant/_Book_k8sInfra $HOME
-  find $HOME/_Book_k8sInfra/ -regex ".*\.\(sh\)" -exec chmod 700 {} \;
-fi
