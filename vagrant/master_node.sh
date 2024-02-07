386f91f9608f2e53052874f0af21fa329c6d27f2
akm4545 <akm4545@naver.com>
2024-02-07 12:12:34 +0900

    마스터 노드 쿠버네티스 설정 sh 파일 및 calico 쿠버네티스 오브젝트 파일 추가

--- vagrant/master_node.sh
+++ vagrant/master_node.sh
@@ -10,5 +10,4 @@ cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
 # config for kubernetes's network 
-kubectl apply -f \
-https://raw.githubusercontent.com/sysnet4admin/IaC/master/manifests/172.16_net_calico.yaml
+kubectl apply -f net_calico.yaml
