91a183713f03ce5971b77b760f6852b46caf325e
akm4545 <akm4545@naver.com>
2024-02-13 14:44:46 +0900

    쿠버네티스 버전에 맞는 도커 버전으로 변경

--- vagrant/install_pkg.sh
+++ vagrant/install_pkg.sh
@@ -7,7 +7,8 @@ yum install git -y
 
 # install docker 
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+yum install docker-ce-18.06.0.ce-3.el7 docker-ce-cli-18.06.0.ce-3.el7 \
+    containerd.io-1.2.6-3.3.el7 -y
 systemctl enable --now docker
 
 # install kubernetes cluster 
