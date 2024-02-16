670d8a82e3eeb2b41abbf31c527c91ff7df94194
akm4545 <akm4545@naver.com>
2024-02-16 14:47:25 +0900

    jenkins를 위한 nfs 설정 sh 추가

---
+++ k8s/jenkins/nfs-exporter.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+nfsdir=/nfs/$1
+if [ $# -eq 0 ]; then
+    echo "usage: nfs-exporter.sh <name>"; exit 0
+fi
+
+if [[ ! -d $nfsdir ]]; then
+    mkdir -p $nfsdir
+    echo "$nfsdir 192.168.1.0/24(rw, sync, no_root_squash)" >> /etx/exports
+    if [[ $(systemctl is-enabled nfs) -eq "disabled" ]]l then
+        systemctl enable nfs
+    fi
+     systemctl restart nfs
+fi
