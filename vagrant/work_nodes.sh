2230f12e31771e7eb53d266ac9e5712f078dc49c
akm4545 <akm4545@naver.com>
2024-03-04 16:14:28 +0900

    도커 호환 버전 변경

--- vagrant/work_nodes.sh
+++ vagrant/work_nodes.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+rm /etc/containerd/config.toml
+systemctl restart containerd
+
 # config for work_nodes only 
 kubeadm join --token 123456.1234567890123456 \
              --discovery-token-unsafe-skip-ca-verification 192.168.1.10:6443
