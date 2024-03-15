a768dd6a287066e80880120d0df06c856e60b4de
akm4545 <akm4545@naver.com>
2024-03-15 16:11:55 +0900

    얼럿매니저 helm, values.yaml 파일 수정

--- k8s/alert-manager/prometheus-alertmanager-install.sh
+++ k8s/alert-manager/prometheus-alertmanager-install.sh
@@ -9,14 +9,13 @@ helm upgrade prometheus edu/prometheus \
 --set server.securityContext.runAsGroup=1000 \
 --set server.securityContext.runAsUser=1000 \
 --set server.service.type="LoadBalancer" \
---set server.baseURL="http://192.168.1.12" \
---set server.service.loadBalancerIP="102.168.1.12" \
+--set server.baseURL="http://192.168.1.15" \
+--set server.service.loadBalancerIP="192.168.1.15" \
 --set server.extraFlags[0]="storage.tsdb.no-lockfile" \
-# 컨피그맵 덮어쓰기 prometheus-notifier-config 해야하는거 아닌가
 --set alertmanager.configMapOverrideName=notifier-config \
 --set alertmanger.securityContext.runAsGroup=1000 \
 --set alertmanger.securityContext.runAsUser=1000 \
 --set alertmanager.service.type="LoadBalancer" \
---set alertmanager.service.loadBalancerIP="192.168.1.14" \
---set alertmanager.baseURL="http://192.168.1.14" \
--f ~/_Book_k8sInfra/ch6/6.5.1/values.yaml
+--set alertmanager.service.loadBalancerIP="192.168.1.17" \
+--set alertmanager.baseURL="http://192.168.1.17" \
+-f ./values.yaml
